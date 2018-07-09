c6b22a4f4ac5b97fb772da9098b0f5f8b66c4452
Josue V. Alorro <joalorro@gmail.com>
2018-07-09 15:47:04 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -36,17 +36,12 @@ def reformat_languages(languages)
   new_hash = {}
   
   languages.each do |style,language_data|
-    #establishing a new hash for each NEW language 
+    
     new_language = {}
     
-    language_data.each do |language,language_type|
-      new_language[language.to_sym]=language_type
-    end
-    binding.pry
+    language_data do |language_name,language_type|
+      language_type.each do |type_class,type|
     
-    language_data.each do |lanuage,language_type|
-      language_type do |type_class,type|
-        
       end
     end
   end
