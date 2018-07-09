1c8f5f62034ea3e9bc306492027ea7f5bebdad6d
Josue V. Alorro <joalorro@gmail.com>
2018-07-09 15:18:20 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -50,8 +50,6 @@ def reformat_languages(languages)
     language_hash.each do |language,data|
       language = language_placeholder
       binding.pry
-      new_language = new_hash[language.to_sym]
-      new_language = {} if new_language == nil 
       
       data.each do |type,type_of_language|
         new_language[]
