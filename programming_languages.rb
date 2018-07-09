103a7ccdab7e8626d317ca910e474f47ad7f42b5
Josue V. Alorro <joalorro@gmail.com>
2018-07-09 15:49:28 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -35,11 +35,14 @@ languages = {
 def reformat_languages(languages)
   new_hash = {}
   
+  #setting the new template for each language in new_hash
   languages.each do |style,language_data|
-    
     new_language = {}
+    language_class = :class 
     
     language_data do |language_name,language_type|
+      language_class = language_name.to_sym
+      new_language[language_name.to_sym] = {}  
       language_type.each do |type_class,type|
     
       end
